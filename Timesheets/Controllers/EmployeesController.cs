84958e7aac1992c4069f2e054b0b7475b4634afa
Uryupin <urdmitriy@yandex.ru>
2021-05-24 12:38:04 +0300

    Lesson5

--- Timesheets/Controllers/EmployeesController.cs
+++ Timesheets/Controllers/EmployeesController.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Timesheets.Domain.Implementation;
+using Timesheets.Domain.Interfaces;
+using Timesheets.Models;
 using Timesheets.Models.Dto;
 
 namespace Timesheets.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class EmployeesController : ControllerBase
     {
-        private readonly EmployeeManager _employeeManager;
+        private readonly IEmployeeManager _employeeManager;
+        private readonly ILoginManager _loginManager;
 
-        public EmployeesController(EmployeeManager employeeManager)
+        public EmployeesController(IEmployeeManager employeeManager, ILoginManager loginManager)
         {
             _employeeManager = employeeManager;
+            _loginManager = loginManager;
         }
 
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromQuery] User user)
+        {
+            var token = await _loginManager.Authenticate(user);
+            if (string.IsNullOrWhiteSpace(token.AccessToken))
+            {
+                return BadRequest(new {message = "Username or password is incorrect"});
+            }
+            
+            return Ok(token.AccessToken);
+        }
+        
+        [Authorize(Roles = "user")]
         [HttpGet("{id}")]
         public IActionResult Get([FromQuery] Guid id)
         {
@@ -25,6 +45,7 @@ namespace Timesheets.Controllers
             return Ok(result);
         }
 
+        [Authorize(Roles = "user")]
         [HttpGet]
         public async Task<IActionResult> GetItems()
         {
@@ -32,6 +53,7 @@ namespace Timesheets.Controllers
             return Ok(result);
         }
 
+        [Authorize(Roles = "user")]
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] EmployeeRequest employee)
         {
@@ -40,7 +62,7 @@ namespace Timesheets.Controllers
             return Ok(id);
         }
 
-
+        [Authorize(Roles = "user")]
         [HttpPut("{id}")]
         public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] EmployeeRequest employee)
         {
